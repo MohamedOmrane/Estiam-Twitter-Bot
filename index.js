d4e8b0eaa2278ef3eccd59880911b62fe828e2d4
Mohamed Omrane <mohamed.omrane@estiam.com>
2023-01-09 21:38:04 +0100

    Fixing the authentication

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const Twit = require('twit');
 // Load API keys and tokens from environment variables
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
-const BEARER_TOKEN = process.env.BEARER_TOKEN;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 // Create a new Twit instance
 const T = new Twit({
-  consumer_key: API_KEY,
-  consumer_secret: API_SECRET,
-  bearer_token: BEARER_TOKEN,
-});
-
+    consumer_key: API_KEY,
+    consumer_secret: API_SECRET,
+    access_token: ACCESS_TOKEN,
+    access_token_secret: ACCESS_TOKEN_SECRET,
+  });
 // Track the "ESTIAM" hashtag in real-time
 const stream = T.stream('statuses/filter', { track: 'ESTIAM' });
 
