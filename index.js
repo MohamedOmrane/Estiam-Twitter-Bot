6c1a4cf666d21550438973c91959455e5f4a0be0
Mohamed Omrane <mohamed.omrane@estiam.com>
2023-01-09 21:03:38 +0100

    First commit

--- index.js
+++ index.js
@@ -15,25 +15,3 @@ const T = new Twit({
 // Track the "ESTIAM" hashtag in real-time
 const stream = T.stream('statuses/filter', { track: 'ESTIAM' });
 
-// Every time a new tweet is posted with the "ESTIAM" hashtag, perform the desired actions
-stream.on('tweet', (tweet) => {
-  // Like the tweet
-  T.post('favorites/create', { id: tweet.id_str }, (err, data, response) => {
-    console.log(`Liked tweet with ID ${tweet.id_str}`);
-  });
-
-  // Retweet the tweet
-  T.post(`statuses/retweet/${tweet.id_str}`, (err, data, response) => {
-    console.log(`Retweeted tweet with ID ${tweet.id_str}`);
-  });
-
-  // Follow the user who posted the tweet, if they have more than 100 followers
-  T.get('users/lookup', { user_id: tweet.user.id_str }, (err, data, response) => {
-    const user = data[0];
-    if (user.followers_count > 100) {
-      T.post('friendships/create', { user_id: user.id_str }, (err, data, response) => {
-        console.log(`Followed user with ID ${user.id_str}`);
-      });
-    }
-  });
-});
